f60fbd5cf1c3caa18c7905161dc33e3b52b4c302
Himaja-Naru <106470633+Himaja-Naru@users.noreply.github.com>
2022-07-30 12:00:04 +0530

    Add files via upload

---
+++ price.js
@@ -0,0 +1,14 @@
+class Pen {
+    constructor(name, color, price){
+        this.name = name;
+        this.color = color; 
+        this.price = price;
+    }
+    
+    showPrice(){
+        console.log(`Price of ${this.name} is ${this.price}`);
+    }
+}
+
+const pen1 = new Pen("Marker", "Blue", "$5");
+pen1.showPrice();
